3e62937159730e3728fa292faaa19b651c632fb5
Ricardo Balbi <rbalbi@gmail.com>
2021-05-20 15:30:02 -0300

    feat(plants): saving plants

--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,12 +1,62 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { formatDistance } from 'date-fns/esm';
+import { ptBR } from 'date-fns/locale';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import waterdropImg from '../assets/waterdrop.png';
 import { Header } from '../components/Header';
+import { PlantCardSecondary } from '../components/PlantCardSecondary';
+import { loadPlant, PlantProps } from '../libs/storage';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function MyPlants() {
+  const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [nextWatered, setNextWatered] = useState<string>();
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const plantsStorage = await loadPlant();
+
+      const nextTime = formatDistance(
+        new Date(plantsStorage[0].dateTimeNotification).getTime(),
+        new Date().getTime(),
+        { locale: ptBR }
+      );
+
+      setNextWatered(
+        `Não esqueça de regar a ${plantsStorage[0].name} ${nextTime}`
+      );
+
+      setMyPlants(plantsStorage);
+      setLoading(false);
+    }
+
+    loadStorageData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Header />
+
+      <View style={styles.spotlight}>
+        <Image style={styles.spotlightImg} source={waterdropImg} />
+
+        <Text style={styles.spotlightText}>{nextWatered}</Text>
+      </View>
+
+      <View style={styles.plants}>
+        <Text style={styles.plantsTitle}>Próximas regadas</Text>
+
+        <FlatList
+          data={myPlants}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <PlantCardSecondary data={item} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ flex: 1 }}
+        />
+      </View>
     </View>
   );
 }
@@ -19,5 +69,31 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingTop: 50,
     backgroundColor: colors.background,
+  },
+
+  spotlight: {
+    backgroundColor: colors.blue_light,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    height: 110,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  spotlightImg: { width: 60, height: 60 },
+
+  spotlightText: {
+    flex: 1,
+    color: colors.blue,
+    paddingHorizontal: 20,
+  },
+
+  plants: { flex: 1, width: '100%' },
+
+  plantsTitle: {
+    fontSize: 24,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    marginVertical: 20,
   },
 });
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { PlantSelect } from '../pages/PlantSelect';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Welcome } from '../pages/Welcome';
 import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -26,11 +27,11 @@ const AppRoutes: React.FC = () => (
     />
     <stackRoutes.Screen name='Confirmation' component={Confirmation} />
 
-    <stackRoutes.Screen name='PlantSelect' component={PlantSelect} />
+    <stackRoutes.Screen name='PlantSelect' component={AuthRoutes} />
 
     <stackRoutes.Screen name='PlantSave' component={PlantSave} />
 
-    <stackRoutes.Screen name='MyPlants' component={MyPlants} />
+    <stackRoutes.Screen name='MyPlants' component={AuthRoutes} />
   </stackRoutes.Navigator>
 );
 
